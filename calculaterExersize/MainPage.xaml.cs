80ca37c748d4d0b833b1c285d8797feced02deb1
User <User@LAB2-2>
2024-01-09 14:53:47 +0200

    Add project files.

---
+++ calculaterExersize/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace calculaterExersize
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
